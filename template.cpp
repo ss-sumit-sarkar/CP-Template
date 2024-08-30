a259aa9f39fdd816ad2cc68e3fc169328d6f3f3f
Sumit Sarkar <sumitsarkar2208@gmail.com>
2024-08-30 14:30:18 +0530

    CP Template

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cin.tie(0);
+#define fr(i,a,b) for(int i=a;i<b;i++)
+#define loop(x,n) for(int i=0;i<n;i++)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+    fast_io;
+    cout<<fixed;
+    cout<<setprecision(10);
+    int t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+    return 0;
+}
